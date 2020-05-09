cde745a0637be9b19fb4e478ea2d6f66a5ad75cb
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-05-09 17:27:31 -0500

    Agregamos un curso al carrito pro emedio de una funcicon llamada insertarCarrito

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Variable
 const carrito = document.getElementById('carrito');
 const cursos = document.getElementById('lista-cursos');
+const listaCursos = document.querySelector('#lista-carrito tbody')
 
 
 
@@ -18,6 +19,7 @@ function comprarCurso(e){
     e.preventDefault();
     console.log(e.target.classList)
     // Se ejecuta cuando el elemento contiene la clase agregar-carrito
+    // (Delegation)
     if(e.target.classList.contains('agregar-carrito')){
         // nos vamos a los 2 padres del elemento al que se sio clic
         const curso = e.target.parentElement.parentElement;
@@ -26,5 +28,29 @@ function comprarCurso(e){
 }
 // lee los datos del curso
 function leerDatosCurso( curso ){
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id')
+    }
     console.log(curso)
+    insertarCarrito(infoCurso)
+}
+
+// Muestra el curso seleccionado en el carito
+function insertarCarrito(curso){
+    console.log(curso)
+    const row = document.createElement('tr')
+    row.innerHTML=`
+        <td>
+            <img src="${curso.imagen}">
+        </td>
+        <td>${curso.titulo}</td>
+        <td>${curso.precio}</td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+        </td>
+    `;
+    listaCursos.appendChild(row)
 }
